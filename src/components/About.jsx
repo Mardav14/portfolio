13705f037164cc3deacb6dd4bacdebffb47aff39
Mardav-Dorle <mardav.dorle22@vit.edu>
2025-03-18 15:44:04 +0530

    update to vanilla-tilt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Tilt from "react-tilt";
+import React, { useEffect, useRef } from "react";
+import VanillaTilt from "vanilla-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -7,33 +7,45 @@ import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+const ServiceCard = ({ index, title, icon }) => {
+  const tiltRef = useRef(null);
+
+  useEffect(() => {
+    if (tiltRef.current) {
+      VanillaTilt.init(tiltRef.current, {
+        max: 45,
+        scale: 1,
+        speed: 450,
+        glare: true,
+        "max-glare": 0.5,
+      });
+    }
+  }, []);
+
+  return (
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      className='xs:w-[250px] w-full'
     >
       <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        ref={tiltRef}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
-        <img
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
+        <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+          <img
+            src={icon}
+            alt='web-development'
+            className='w-16 h-16 object-contain'
+          />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
+          <h3 className='text-white text-[20px] font-bold text-center'>
+            {title}
+          </h3>
+        </div>
       </div>
     </motion.div>
-  </Tilt>
-);
+  );
+};
 
 const About = () => {
   return (
